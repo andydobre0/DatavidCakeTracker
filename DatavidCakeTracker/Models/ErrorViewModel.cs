3015eae39ab29f6685e1eab0cc853c76db8fb6de
andydobre0 <dobreandy1@gmail.com>
2024-06-12 18:47:41 +0300

    Add project files.

---
+++ DatavidCakeTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DatavidCakeTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
